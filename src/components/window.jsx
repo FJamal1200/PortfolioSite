d7eff03ac8a4d8106dbca8cfbeb766493c774498
FJamal <farshidjamal2@gmail.com>
2025-10-16 00:46:10 -0400

    Fixed image paths for Lightbox

--- src/components/window.jsx
+++ src/components/window.jsx
@@ -174,7 +174,7 @@ export default function Window({ id, onClose }) {
                       "garret.png",
                       "onolaband.png",
                       "turtwig.png",
-                    ].map((f) => ({ src: `/${f}` }))
+                    ].map((f) => ({ src: `/${imagepath}${f}` }))
                   );
                   setLightboxIndex(i);
                   setLightboxOpen(true);
